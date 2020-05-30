cfdcee4965e322e2ffbb81b879b6656ef288ae74
Joe Geldart <joe@joegeldart.com>
2020-05-30 21:40:40 +0100

    Add support for ontology design patterns

--- notation4/__init__.py
+++ notation4/__init__.py
@@ -289,19 +289,46 @@ def variable_scope_check(model, metamodel):
         return (
             hasattr(x, "name") and x.name is not None
             and (x.name.startswith("?") or x.name.startswith("!"))
-            and get_parent_of_type("Formula", x) is None
+            and (
+                get_parent_of_type("Formula", x) is None
+                and get_parent_of_type("ClassPatternDecl", x) is None
+                and get_parent_of_type("PropertyPatternDecl", x) is None
+                and get_parent_of_type("AttributePatternDecl", x) is None
+                and get_parent_of_type("DatatypePatternDecl", x) is None
+                and get_parent_of_type("IndividualPatternDecl", x) is None
+                and get_parent_of_type("RestrictionPatternDecl", x) is None
+                and get_parent_of_type("LiteralPatternDecl", x) is None
+            )
         )
 
     def _ref_decider(x):
         return (
             ((hasattr(x, "universal_var") and x.universal_var is not None) or (hasattr(x, "existential_var") and x.existential_var is not None))
-            and get_parent_of_type("Formula", x) is None
+            and (
+                get_parent_of_type("Formula", x) is None
+                and get_parent_of_type("ClassPatternDecl", x) is None
+                and get_parent_of_type("PropertyPatternDecl", x) is None
+                and get_parent_of_type("AttributePatternDecl", x) is None
+                and get_parent_of_type("DatatypePatternDecl", x) is None
+                and get_parent_of_type("IndividualPatternDecl", x) is None
+                and get_parent_of_type("RestrictionPatternDecl", x) is None
+                and get_parent_of_type("LiteralPatternDecl", x) is None
+            )
         )
 
     def _headed_list_decider(x):
         return (
             (x.__class__.__name__ == "HeadedList" or x.__class__.__name__.startswith("Infix"))
-            and get_parent_of_type("Formula", x) is None
+            and (
+                get_parent_of_type("Formula", x) is None
+                and get_parent_of_type("ClassPatternDecl", x) is None
+                and get_parent_of_type("PropertyPatternDecl", x) is None
+                and get_parent_of_type("AttributePatternDecl", x) is None
+                and get_parent_of_type("DatatypePatternDecl", x) is None
+                and get_parent_of_type("IndividualPatternDecl", x) is None
+                and get_parent_of_type("RestrictionPatternDecl", x) is None
+                and get_parent_of_type("LiteralPatternDecl", x) is None
+            )
         )
 
     issues = get_children(_decl_decider, model)
@@ -312,9 +339,9 @@ def variable_scope_check(model, metamodel):
         if hasattr(first_error, "name"):
             var = "Variable '{}'".format(first_error.name)
         elif hasattr(first_error, "universal_var"):
-            var = "Variable '?{}'".format(first_error.universal_var)
+            var = "Variable '{}'".format(first_error.universal_var)
         elif hasattr(first_error, "existential_var"):
-            var = "Variable '!{}'".format(first_error.existential_var)
+            var = "Variable '{}'".format(first_error.existential_var)
         elif hasattr(first_error, "head"):
             var = "Headed list '{}'".format(first_error.head.ref.name)
         else:
@@ -323,7 +350,7 @@ def variable_scope_check(model, metamodel):
         line, col = parser.pos_to_linecol(
             first_error._tx_position
             )
-        message = "{} used outside a formula in '{}'".format(
+        message = "{} used outside a formula or pattern in '{}'".format(
             var,
             str(first_error)
             )
